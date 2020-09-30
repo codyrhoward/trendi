81b22fd15626ab1998fc548ed437b57681e196ae
codyrhoward <codyrhoward19@gmail.com>
2020-09-30 12:23:40 -0400

    created MessageSender.js component for the feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,6 +6,7 @@ export default function Feed() {
     return (
         <div className="feed">
             <StoryReel />
+            <MessageSender />
         </div>
     )
 }
---
+++ src/components/MesageSender.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './MessageSender.css'
+
+export default function MesageSender() {
+    return (
+        <div className="messageSender">
+            
+        </div>
+    )
+}
