da5c72a3440b30e077dde8e459b7eb34d14133ee
codyrhoward <codyrhoward19@gmail.com>
2020-09-30 14:06:45 -0400

    finished messageSender block. Styled messageSender. Started on the Posts.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Feed.css'
 import StoryReel from './StoryReel'
+import MessageSender from './MesageSender'
+import Post from './Post'
 
 export default function Feed() {
     return (
         <div className="feed">
             <StoryReel />
             <MessageSender />
+            <Post />
+            <Post />
+            <Post />
         </div>
     )
 }
--- src/components/MesageSender.js
+++ src/components/MesageSender.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './MessageSender.css'
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import { Avatar } from '@material-ui/core'
 
 export default function MesageSender() {
+const [input, setInput] = useState('')
+const [imageUrl, setImageUrl] = useState('')
+
+const handleSubmit = event => {
+    event.preventDefault()
+    setInput('')
+    setImageUrl('')
+}
+
     return (
         <div className="messageSender">
-            
+            <div className="messageSender__top">
+                <Avatar />
+                <form>
+                    <input 
+                     value={input}
+                     onChange={(event) => setInput(event.target.value)}
+                     className="messageSender__input"
+                     type="text" 
+                     placeholder={`What's on your mind?`} 
+                    />
+                    <input 
+                     value={imageUrl}
+                     onChange={(event) => setImageUrl(event.target.value)}
+                     className=""
+                     type="text"
+                     placeholder="Image URL (Optional)" 
+                    />
+                    <button onClick={handleSubmit} type="submit">
+                        Hidden submit
+                    </button>
+                </form>
+            </div>
+
+            <div className="messageSender__bottom">
+                <div className="messageSender__option">
+                    <VideocamIcon style={{ color: 'coral' }} />
+                    <h3>Go Live</h3>
+                </div>
+                <div className="messageSender__option">
+                    <PhotoLibraryIcon style={{ color: 'lightBlue' }} />
+                    <h3>Photo/Video</h3>
+                </div>
+                <div className="messageSender__option">
+                    <InsertEmoticonIcon style={{ color: 'gold' }} />
+                    <h3>Mood</h3>
+                </div>
+            </div>
         </div>
     )
 }
